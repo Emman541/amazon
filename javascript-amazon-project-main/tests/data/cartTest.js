73042e455948566ee6037d3dd66a5d0e08bae320
Fappy <faparusigbotemi@gmail.com>
2024-08-19 13:59:33 +0100

    move test-simple into tests

--- javascript-amazon-project-main/tests/data/cartTest.js
+++ javascript-amazon-project-main/tests/data/cartTest.js
@@ -1,7 +1,6 @@
-import { addToCart, cart, loadFromStorage} from "../../data/cart.js";
+import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
 
-
-describe('testsuite: addToCart', () => {
+describe('test suite: addToCart', () => {
   it('adds an existing product to the cart', () => {
     spyOn(localStorage, 'setItem');
 
@@ -13,16 +12,15 @@ describe('testsuite: addToCart', () => {
       }]);
     });
     loadFromStorage();
-    
+
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart.length).toEqual(1)
+    expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart[0].quantity).toEqual(2)
-  })
+    expect(cart[0].quantity).toEqual(2);
+  });
 
-  it('add a new product to the cart', () => {
-
+  it('adds a new product to the cart', () => {
     spyOn(localStorage, 'setItem');
 
     spyOn(localStorage, 'getItem').and.callFake(() => {
@@ -30,12 +28,10 @@ describe('testsuite: addToCart', () => {
     });
     loadFromStorage();
 
-
-
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart.length).toEqual(1)
+    expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart[0].quantity).toEqual(1)
-  })
-})
+    expect(cart[0].quantity).toEqual(1);
+  });
+});
