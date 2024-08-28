8d5c77cd9fb16a669d7c4b1572e84a3e7c8b8e9a
Fappy <faparusigbotemi@gmail.com>
2024-08-28 11:20:08 +0100

    convert from promises to async await

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -5,19 +5,37 @@ import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js'
 // import  "../data/backend-practice.js";
 
-Promise.all([
-  loadProductsFetch(), 
-  new Promise ((resolve) => {
-    loadCart(() => {
-      resolve();
-    })
-  })
+async function loadPage() {
 
-]).then((values) => {
-  console.log(values);
+
+  await loadProductsFetch();
+
+  await new Promise ((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+  });
+
   renderOrderSummary();
-  renderPaymentSummary();  
-})
+  renderPaymentSummary();
+
+}
+
+loadPage()
+
+// Promise.all([
+//   loadProductsFetch(), 
+//   new Promise ((resolve) => {
+//     loadCart(() => {
+//       resolve();
+//     })
+//   })
+
+// ]).then((values) => {
+//   console.log(values);
+//   renderOrderSummary();
+//   renderPaymentSummary();  
+// })
 
 // new Promise((resolve) => {
 //   loadProducts(() => {
