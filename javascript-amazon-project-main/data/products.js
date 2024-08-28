5285a06defb3c5916f509f77502e15dcfda500df
Fappy <faparusigbotemi@gmail.com>
2024-08-28 11:48:43 +0100

    learn error handling

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -100,6 +100,10 @@ export function loadProductsFetch() {
     console.log('load products');
 
   
+  }).catch((error) => {
+    
+    console.log('unexpected error. please try again later');
+
   });
 
   return promise
@@ -122,11 +126,14 @@ export function loadProducts(fun) {
       console.log('load products');
 
     fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('unexpected error. please try again later');
   })
   xhr.open('GET', 'https://supersimplebackend.dev/products')
   xhr.send()
 }
-
 // }
 // object3.method()
 // export const products = [
--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -6,15 +6,22 @@ import { loadCart } from "../data/cart.js";
 // import  "../data/backend-practice.js";
 
 async function loadPage() {
+  try {
+    // throw 'error1'
 
 
-  await loadProductsFetch();
+    await loadProductsFetch();
 
-  await new Promise ((resolve) => {
+    const value = await new Promise ((resolve, reject) => {
+      // throw 'error2'
       loadCart(() => {
-        resolve();
+        // reject('error3')
+        resolve('value3');
       });
-  });
+    }); 
+  } catch (error) {
+    console.log('unexpected error. please try again later');
+  }
 
   renderOrderSummary();
   renderPaymentSummary();
